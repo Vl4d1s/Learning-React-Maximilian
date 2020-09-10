c817a3df85085f1cf98bc2df1590d1bf13d0c282
Vladis Markin <vladismarkin@gmail.com>
2020-09-10 22:53:33 +0300

    converting to hucks state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, My first react app</h1>
-        <Person name="Vladis" age="29">
-          This is "children" text
-        </Person>
-        <Person name="Roman" age="26" />
-        <Person name="Yankel" age="26" />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [personState, setPersonState] = useState({
+    person: [
+      { name: "Vladis", age: 29 },
+      { name: "Roman", age: 26 },
+      { name: "Yankel", age: 26 },
+    ],
+  });
+
+  const swichNameHandler = () => {
+    setPersonState({
+      person: [
+        { name: "Vladimir", age: 29 },
+        { name: "Roman", age: 26 },
+        { name: "Yankel", age: 25 },
+      ],
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, My first react app</h1>
+      <button onClick={swichNameHandler}>Swich text</button>
+      <Person name={personState.person[0].name} age={personState.person[0].age}>
+        This is "children" text
+      </Person>
+      <Person
+        name={personState.person[1].name}
+        age={personState.person[1].age}
+      />
+      <Person
+        name={personState.person[2].name}
+        age={personState.person[2].age}
+      />
+    </div>
+  );
+};
 
 export default App;
