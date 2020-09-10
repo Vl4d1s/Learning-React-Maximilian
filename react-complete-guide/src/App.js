65924268104438c94c7f68e3e8b2a5f7547b8419
Vladis Markin <vladismarkin@gmail.com>
2020-09-10 22:05:22 +0300

    Adding props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,8 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, My firest react app</h1>
-        <Person />
+        <h1>Hi, My first react app</h1>
+        <Person name="Vladis" age="29">
+          Some text here
+        </Person>
+        <Person name="Roman" age="26" />
+        <Person name="Yankel" age="26" />
       </div>
     );
   }
--- react-complete-guide/src/Person/person.js
+++ react-complete-guide/src/Person/person.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const person = () => {
-  return <p>I'm a Person</p>;
+const person = (props) => {
+  return (
+    <p>
+      I'm {props.name} and i am {Math.floor(Math.random() * 30)} years old
+    </p>
+  );
 };
 
 export default person;
