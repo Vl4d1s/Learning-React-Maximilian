93b00c08ab34c7873a25fa4bf11fe5c2691b699b
Vladis Markin <vladismarkin@gmail.com>
2020-09-11 19:12:23 +0300

    Adding Two Way Binding

--- react-complete-guide/src/Person/person.js
+++ react-complete-guide/src/Person/person.js
@@ -7,6 +7,7 @@ const person = (props) => {
         I'm {props.name} and i am {Math.floor(Math.random() * 30)} years old
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
