650e8732391762b85cac8b30635b368856d9ec25
Vladis Markin <vladismarkin@gmail.com>
2020-09-11 17:47:51 +0300

    trying by myself

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
+import person from "./Person/person";
 import Person from "./Person/person";
 
 const App = () => {
   const [personState, setPersonState] = useState({
     person: [
-      { name: "Vladis", age: 29 },
-      { name: "Roman", age: 26 },
-      { name: "Yankel", age: 26 },
+      { name: "Vladis", age: "29" },
+      { name: "Roman", age: "26" },
+      { name: "Yankel", age: "25" },
     ],
   });
 
-  const swichNameHandler = () => {
+  const changePersonHandler = () => {
     setPersonState({
       person: [
-        { name: "Vladimir", age: 29 },
-        { name: "Roman", age: 26 },
-        { name: "Yankel", age: 25 },
+        { name: "Vladimir", age: "29" },
+        { name: "Roman", age: "26" },
+        { name: "Yankel", age: "27" },
       ],
     });
   };
 
   return (
-    <div className="App">
-      <h1>Hi, My first react app</h1>
-      <button onClick={swichNameHandler}>Swich text</button>
-      <Person name={personState.person[0].name} age={personState.person[0].age}>
-        This is "children" text
-      </Person>
+    <div>
       <Person
-        name={personState.person[1].name}
-        age={personState.person[1].age}
+        name={personState.person[0].name}
+        age={personState.person[0].age}
       />
+      <Person name={personState.person[1].name} age={personState.person[1].age}>
+        Some text here
+      </Person>
       <Person
         name={personState.person[2].name}
         age={personState.person[2].age}
       />
+      <button onClick={changePersonHandler}>Click me!</button>
     </div>
   );
 };
